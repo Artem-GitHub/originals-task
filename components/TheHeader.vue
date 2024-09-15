f2fef285e115677ede68f32699df1c0639ee4670
Artem <artyom9302@gmail.com>
2024-09-15 17:13:14 +0300

    created header & base button

---
+++ components/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <div class="header__logo">Logo</div>
+
+    <div class="header__actions">
+      <BaseButton>Create</BaseButton>
+    </div>
+  </header>
+</template>
+
+<style lang="sass" scoped>
+.header
+  display: flex
+  align-items: center
+  justify-content: space-between
+  height: 72px
+  background-color: $primary-800
+  padding: 0 32px
+
+  &__logo
+    text-transform: uppercase
+    font-weight: 600
+    font-size: 32px
+    color: $white
+
+  &__actions
+    display: inline-flex
+    align-items: center
+</style>
---
+++ components/base/BaseButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    class="base-button"
+    type="button"
+  >
+    <slot />
+  </button>
+</template>
+
+<style lang="sass" scoped>
+.base-button
+  display: flex
+  align-items: center
+  justify-content: center
+  height: 32px
+  background-color: $primary-400
+  border-radius: 8px
+  transition: background-color 0.25s ease
+  font-size: 16px
+  color: $white
+  padding: 0 12px
+
+  &:hover
+    background-color: $primary-300
+
+  &:active
+    background-color: $primary-500
+</style>
